daddf4bc2289b7ac5ce4aaeba34f51a2cf6885b4
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-06-04 10:53:57 +0530

    Initial commit

---
+++ MedEase_website/client/src/pages/startpage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+
+import React from 'react'
+
+function startpage() {
+  return (
+    <div>
+      <nav>
+        
+      </nav>
+    </div>
+  )
+}
+
+export default startpage;
